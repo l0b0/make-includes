b1406d1b7ca16486992a472bbd524874a461f0cb
Victor Engmark <victor.engmark@gmail.com>
2014-02-07 22:02:39 +0000

    Support METHOD=git to check only files in the Git index or working tree

--- python.mk
+++ python.mk
@@ -1,4 +1,11 @@
-FIND := /usr/bin/find
+# Use METHOD=git to check only files in the Git index or working tree
+
+METHOD = find
+
+method_find := /usr/bin/find . -type f -name '*.py' -exec printf '%s\0' {} +
+method_git := git ls-files -z '*.py'
+
+find = $(method_$(METHOD))
 
 # Check Python code for all sorts of lint
 .PHONY: python-lint
@@ -6,16 +13,16 @@ python-lint: python-pep8 python-pychecker python-pylint python-pyflakes
 
 .PHONY: python-pep8
 python-pep8:
-	$(FIND) . -type f -name '*.py' -exec pep8 --max-line-length 120 {} +
+	$(find) | xargs -0 pep8 --max-line-length 120
 
 .PHONY: python-pychecker
 python-pychecker:
-	$(FIND) . -type f -name '*.py' -exec pychecker {} +
+	$(find) | xargs -0 pychecker
 
 .PHONY: python-pylint
 python-pylint:
-	$(FIND) . -type f -name '*.py' -exec pylint {} +
+	$(find) | xargs -0 pylint
 
 .PHONY: python-pyflakes
 python-pyflakes:
-	$(FIND) . -type f -name '*.py' -exec pyflakes {} +
+	$(find) | xargs -0 pyflakes
