9614f410433dfb943b07786b98a68aa7148ca7bd
Victor Engmark <victor.engmark@gmail.com>
2014-08-25 22:08:45 +0100

    Enable building outside of source tree
    This avoids cluttering the directory so that `pep8` doesn't see any
    third party code.

--- python.mk
+++ python.mk
@@ -9,7 +9,7 @@ endif
 PYTHON_VERSION ?= $(shell python --version | cut -d ' ' -f 2 | cut -d '.' -f 1-3)
 PYTHON_NAME ?= Python-$(PYTHON_VERSION)
 PYTHON_SOURCE_DIRECTORY ?= $(PYTHON_BUILD_DIRECTORY)/$(PYTHON_NAME)
-PYTHON_PREFIX ?= $(CURDIR)/build/$(PYTHON_NAME)-install
+PYTHON_PREFIX ?= $(realpath $(PYTHON_BUILD_DIRECTORY))/$(PYTHON_NAME)-install
 PYTHON_TARBALL ?= $(PYTHON_NAME).tgz
 PYTHON_TARBALL_PATH ?= $(PYTHON_BUILD_DIRECTORY)/$(PYTHON_TARBALL)
 PYTHON_DOWNLOAD_URL ?= https://www.python.org/ftp/python/$(PYTHON_VERSION)/$(PYTHON_TARBALL)
@@ -25,7 +25,7 @@ VIRTUALENV_TARBALL_PATH ?= $(VIRTUALENV_BUILD_DIRECTORY)/$(VIRTUALENV_TARBALL)
 VIRTUALENV_DOWNLOAD_URL ?= https://pypi.python.org/packages/source/v/virtualenv/$(VIRTUALENV_TARBALL)
 VIRTUALENV_EXECUTABLE ?= $(VIRTUALENV_SOURCE_DIRECTORY)/virtualenv.py
 
-VIRTUALENV_DIRECTORY ?= virtualenv-$(PYTHON_VERSION)
+VIRTUALENV_DIRECTORY ?= $(PYTHON_BUILD_DIRECTORY)/virtualenv-$(PYTHON_VERSION)
 
 PYTHON_BUILD_DIRECTORY ?= build/python
 python_version_numbers = $(wordlist 1,3,$(subst ., ,$(PYTHON_VERSION)))
@@ -34,7 +34,7 @@ python_version_minor = $(word 2,$(python_version_numbers))
 python_version_patch = $(word 3,$(python_version_numbers))
 python_short_version = $(python_version_major).$(python_version_minor)
 
-VIRTUALENV_BUILD_DIRECTORY ?= build/virtualenv
+VIRTUALENV_BUILD_DIRECTORY ?= $(PYTHON_BUILD_DIRECTORY)/virtualenv
 virtualenv_activate = $(VIRTUALENV_DIRECTORY)/bin/activate
 
 .PHONY: python-pep8
