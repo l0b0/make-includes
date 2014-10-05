7b9e24678ea1c96278a5d7c7c309aa1d462c65ff
Victor Engmark <victor.engmark@gmail.com>
2014-10-05 10:24:06 +0100

    Use variables for non-shell command paths

--- posix-shell.mk
+++ posix-shell.mk
@@ -1,12 +1,17 @@
 # Use METHOD=git to check only files in the Git index or working tree
+FIND = /usr/bin/find
+GIT = /usr/bin/git
+SH = /usr/bin/sh
+XARGS = /usr/bin/xargs
+
 METHOD = find
 ifeq ($(METHOD),git)
-	posix_files_run = git ls-files -z '*.sh' | xargs -0
+	posix_files_run = $(GIT) ls-files -z '*.sh' | $(XARGS) -0
 else ifeq ($(METHOD),find)
-	posix_files_run = /usr/bin/find . -type f -name '*.sh' -exec printf '%s\0' {} + | xargs -0
+	posix_files_run = $(FIND) . -type f -name '*.sh' -exec printf '%s\0' {} + | $(XARGS) -0
 endif
 
 # Check Python code for all sorts of lint
 .PHONY: posix-shell-test-syntax
 posix-shell-test-syntax:
-	$(posix_files_run) sh -o noexec
+	$(posix_files_run) $(SH) -o noexec
--- python.mk
+++ python.mk
@@ -1,12 +1,23 @@
+FIND = /usr/bin/find
+GIT = /usr/bin/git
+LN = /usr/bin/ln
+MAKE = /usr/bin/make
+MKDIR = /usr/bin/mkdir
+PYTHON = /usr/bin/python
+TAR = /usr/bin/tar
+VIRTUALENV = /usr/bin/virtualenv
+WGET = /usr/bin/wget
+XARGS = /usr/bin/xargs
+
 # Use METHOD=git to check only files in the Git index or working tree
 METHOD = find
 ifeq ($(METHOD),git)
-	python_files_run = git ls-files -z '*.py' | xargs -0
+	python_files_run = $(GIT) ls-files -z '*.py' | $(XARGS) -0
 else ifeq ($(METHOD),find)
-	python_files_run = /usr/bin/find . -type f -name '*.py' -exec printf '%s\0' {} + | xargs -0
+	python_files_run = $(FIND) . -type f -name '*.py' -exec printf '%s\0' {} + | $(XARGS) -0
 endif
 
-PYTHON_VERSION ?= $(shell python --version | cut -d ' ' -f 2 | cut -d '.' -f 1-3)
+PYTHON_VERSION ?= $(shell $(PYTHON) --version | cut -d ' ' -f 2 | cut -d '.' -f 1-3)
 PYTHON_NAME ?= Python-$(PYTHON_VERSION)
 PYTHON_SOURCE_DIRECTORY ?= $(PYTHON_BUILD_DIRECTORY)/$(PYTHON_NAME)
 PYTHON_PREFIX ?= $(realpath $(PYTHON_BUILD_DIRECTORY))/$(PYTHON_NAME)-install
@@ -17,7 +28,7 @@ PYTHON_MAKEFILE ?= $(PYTHON_SOURCE_DIRECTORY)/Makefile
 PYTHON_SOURCE_EXECUTABLE ?= $(PYTHON_SOURCE_DIRECTORY)/python
 PYTHON_EXECUTABLE ?= $(PYTHON_PREFIX)/bin/python$(python_short_version)
 
-VIRTUALENV_VERSION ?= $(shell virtualenv --version)
+VIRTUALENV_VERSION ?= $(shell $(VIRTUALENV) --version)
 VIRTUALENV_NAME ?= virtualenv-$(VIRTUALENV_VERSION)
 VIRTUALENV_SOURCE_DIRECTORY ?= $(VIRTUALENV_BUILD_DIRECTORY)/$(VIRTUALENV_NAME)
 VIRTUALENV_TARBALL ?= $(VIRTUALENV_NAME).tar.gz
@@ -41,24 +52,24 @@ python-pep8:
 	$(python_files_run) pep8 $(PEP8_OPTIONS)
 
 $(PYTHON_TARBALL_PATH): $(PYTHON_BUILD_DIRECTORY)
-	wget --timestamp --directory-prefix $(PYTHON_BUILD_DIRECTORY) $(PYTHON_DOWNLOAD_URL)
+	$(WGET) --timestamp --directory-prefix $(PYTHON_BUILD_DIRECTORY) $(PYTHON_DOWNLOAD_URL)
 
 # Two targets in one to work around 1-second resolution on Make timestamps
 $(PYTHON_MAKEFILE): $(PYTHON_TARBALL_PATH)
-	tar --extract --gzip --directory $(dir $(PYTHON_TARBALL_PATH)) --file $(PYTHON_TARBALL_PATH)
+	$(TAR) --extract --gzip --directory $(dir $(PYTHON_TARBALL_PATH)) --file $(PYTHON_TARBALL_PATH)
 	cd $(PYTHON_SOURCE_DIRECTORY) && ./configure --prefix $(PYTHON_PREFIX)
 
 $(PYTHON_SOURCE_EXECUTABLE): $(PYTHON_MAKEFILE)
-	make -C $(PYTHON_SOURCE_DIRECTORY)
+	$(MAKE) -C $(PYTHON_SOURCE_DIRECTORY)
 
 $(PYTHON_EXECUTABLE): $(PYTHON_SOURCE_EXECUTABLE)
-	make -C $(PYTHON_SOURCE_DIRECTORY) install
+	$(MAKE) -C $(PYTHON_SOURCE_DIRECTORY) install
 
 $(VIRTUALENV_TARBALL_PATH): $(VIRTUALENV_BUILD_DIRECTORY)
-	wget --timestamp --directory-prefix $(VIRTUALENV_BUILD_DIRECTORY) $(VIRTUALENV_DOWNLOAD_URL)
+	$(WGET) --timestamp --directory-prefix $(VIRTUALENV_BUILD_DIRECTORY) $(VIRTUALENV_DOWNLOAD_URL)
 
 $(VIRTUALENV_SOURCE_DIRECTORY): $(VIRTUALENV_TARBALL_PATH)
-	tar --extract --gzip --directory $(dir $(VIRTUALENV_TARBALL_PATH)) --file $(VIRTUALENV_TARBALL_PATH)
+	$(TAR) --extract --gzip --directory $(dir $(VIRTUALENV_TARBALL_PATH)) --file $(VIRTUALENV_TARBALL_PATH)
 
 $(VIRTUALENV_EXECUTABLE): $(VIRTUALENV_SOURCE_DIRECTORY)
 
@@ -67,7 +78,7 @@ $(VIRTUALENV_DIRECTORY): $(PYTHON_EXECUTABLE) $(VIRTUALENV_EXECUTABLE)
 
 .PHONY: virtualenv
 virtualenv: $(VIRTUALENV_DIRECTORY)
-	ln -fns $(VIRTUALENV_DIRECTORY) virtualenv
+	$(LN) -fns $(VIRTUALENV_DIRECTORY) virtualenv
 
 .PHONY: clean-python
 clean-python: clean-python-build clean-python-virtualenv
@@ -81,4 +92,4 @@ clean-python-virtualenv:
 	$(RM) -r $(VIRTUALENV_DIRECTORY) virtualenv
 
 $(PYTHON_BUILD_DIRECTORY) $(VIRTUALENV_BUILD_DIRECTORY):
-	mkdir --parent $@
+	$(MKDIR) --parent $@
